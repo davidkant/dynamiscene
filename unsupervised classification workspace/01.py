8289135370b55893c99712f8eb147d9174ad0dcc
David Kant <david.kant@gmail.com>
2016-08-27 11:39:56 -0700

    add melspectrogram features

--- unsupervised classification workspace/01.py
+++ unsupervised classification workspace/01.py
@@ -7,31 +7,35 @@
   -> variable / thresholded n_clusters
   -> segment width and hop rather than frame-to-frame
   -> object function cosine distance on mel frequency spectrum
+  -> add panogram data (need stereo input)
 
  """
 
 import numpy as np
 import librosa
 import sklearn
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
+import scikits.audiolab
 
 # params
+FERP_audio_folder = "../../FERP/audio"
 filename = '/selected/FERP_08_clip.wav'
 start, end = 0.0, 35.0
 n_clusters = 5
 
 # load audio
-y, sr, pcm = scikits.audiolab.wavread("../../audio/{0}".format(filename))
+y, sr, pcm = scikits.audiolab.wavread("{0}/{1}".format(FERP_audio_folder, filename))
 
 # select region
 y = y[start*sr: end*sr]
 
-# MFCCs
-X_mel = librosa.feature.mfcc(y=y, sr=sr)
+# features
+X = librosa.feature.mfcc(y=y, sr=sr) # MFCCs
+# X = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128) # Mel Spectrum
 
 # cluster
 est = sklearn.cluster.KMeans(n_clusters=n_clusters)
-est.fit(X_mel.T)
+est.fit(X.T)
 
 # plot
 plt.figure()
